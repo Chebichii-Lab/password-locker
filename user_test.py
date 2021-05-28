863427bdc52b4b84dcde40935f1d7d67082ab6a6
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-05-28 22:01:48 +0300

    added test case in the file

--- user_test.py
+++ user_test.py
@@ -9,4 +9,16 @@ class TestUser(unittest.TestCase):
         '''
          set up method to be run before each test case
         '''
-        self.new_user= User("natcase",)
+        self.new_user= User("natcase","chebichii1") # new user created
+
+    def test__init(self):
+        '''
+        to test if the object has been initialized
+        '''
+        self.assertEqual(self.new_user.username, "natcase")
+        self.assertEqual(self.new_user.password, "chebichii1")
+
+
+if __name__ == '__main__':
+    unittest.main()        
+
